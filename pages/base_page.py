a99a88798a6985e5cace6d78270dbb2b6de4c1c1
Evgeniy T <e737business@gmail.com>
2024-12-10 17:16:56 +0700

    v 1.23 - add base page, fix naming

---
+++ pages/base_page.py
@@ -0,0 +1,14 @@
+import logging
+
+
+class BasePage:
+    UNIQUE_ELEMENT_LOC = None
+
+    def __init__(self, browser):
+        self.unique_element = None
+        self.browser = browser
+        self.page_name = None
+
+    def wait_for_open(self):
+        logging.info("Open page")
+        self.unique_element.presence_of_element()
